6951f4e40be1b4c1a765b7c3ee8420da10b081f2
progfay <shun1856shun@gmail.com>
2019-09-05 18:55:23 +0900

    chore(solve.js): support timestamp

--- solve.js
+++ solve.js
@@ -4,8 +4,9 @@ const { JSDOM } = require('jsdom')
 fetch('http://localhost:3000')
   .then(response => response.text())
   .then(html => new JSDOM(html))
-  .then(({ window }) => (
-    Array.from(window.document.querySelectorAll('span'))
+  .then(({ window }) => ({
+    timestamp: window.document.querySelector('input[name="timestamp"]').value,
+    pass: Array.from(window.document.querySelectorAll('span'))
       .map(span => [
         parseInt(window.getComputedStyle(span).order, 10),
         span.innerHTML
@@ -13,13 +14,13 @@ fetch('http://localhost:3000')
       .sort((a, b) => a[0] - b[0])
       .map(v => v[1])
       .join('')
-  ))
-  .then(pass => fetch(
+    }))
+  .then(({ timestamp, pass }) => fetch(
     'http://localhost:3000/cert',
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ pass })
+      body: JSON.stringify({ timestamp, pass })
     }
   ))
   .then(response => response.text())
